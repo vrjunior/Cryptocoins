5ac0a4c1caa08db6feb4df9307e6b558a1b5486a
Valmir Jr <valmirmassoni@gmail.com>
2016-09-25 00:51:20 -0300

    Algumas correções na Ui

--- app/src/main/java/us/guihouse/criptocoins/CryptocoinAdapter.java
+++ app/src/main/java/us/guihouse/criptocoins/CryptocoinAdapter.java
@@ -2,7 +2,9 @@ package us.guihouse.criptocoins;
 
 import android.content.Context;
 import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
 import android.media.Image;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,6 +12,8 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 import us.guihouse.criptocoins.models.CryptoCoin;
@@ -21,6 +25,8 @@ public class CryptocoinAdapter extends RecyclerView.Adapter<CryptocoinAdapter.Cu
 
     private List<CryptoCoin> cryptocoinItens;
     private Context mContext;
+    public static final String COINS_LOGOS_FOLDER = "coins_logos/";
+    public static final String POS_FIX_IMG_LOGOS = ".png";
 
     public CryptocoinAdapter(Context context, int resource, List<CryptoCoin> objects) {
         this.cryptocoinItens = objects;
@@ -40,11 +46,23 @@ public class CryptocoinAdapter extends RecyclerView.Adapter<CryptocoinAdapter.Cu
     public void onBindViewHolder(CustomViewHolder holder, int position) {
         CryptoCoin cryptoCoin = cryptocoinItens.get(position);
 
-        //TODO: setar a imagem logo da moeda
+        InputStream ims;
+        try {
+            ims = this.mContext.getAssets().open(COINS_LOGOS_FOLDER + cryptoCoin.getId() + POS_FIX_IMG_LOGOS);
+            Drawable d = Drawable.createFromStream(ims, null);
+            holder.ivCoinLogo.setImageDrawable(d);
+        }catch (IOException e) {} //Find the image in a fullhd screen if you can! HU3 HU3 BR
+
         holder.tvCoinName.setText(cryptoCoin.getName());
         holder.tvCoinSymbol.setText(cryptoCoin.getSymbol());
-        holder.tvPrice.setText(String.format("$ %.2f", cryptoCoin.getPriceUsd()));
+        holder.tvPrice.setText(String.format("$%.2f", cryptoCoin.getPriceUsd()));
         holder.tvPercentChange24h.setText(String.format("%.2f %%", cryptoCoin.getPercentChange24h()));
+        if(cryptoCoin.getPercentChange24h() >= 0) {
+            holder.tvPercentChange24h.setTextColor(ContextCompat.getColor(mContext, R.color.green_text));
+        }
+        else {
+            holder.tvPercentChange24h.setTextColor(ContextCompat.getColor(mContext, R.color.red_text)); // Como as views são recicladas, é necessário esse else!
+        }
 
     }
 
