27da1c9ff5621de0fe20a1c127a4496149abe870
Valmir Jr <valmirmassoni@gmail.com>
2016-09-29 09:15:05 -0300

    fix

--- app/src/main/java/us/guihouse/criptocoins/repositories/TickerRepository.java
+++ app/src/main/java/us/guihouse/criptocoins/repositories/TickerRepository.java
@@ -15,6 +15,10 @@ import us.guihouse.criptocoins.models.Ticker;
 public class TickerRepository {
     private SQLiteDatabase database;
     ContentValues content;
+    public static final int FAVORITE_TRUE = 1;
+    public static final int FAVORITE_FALSE = 0;
+    public static final String CRYPTOCOINS_TABLE = "cryptocoins";
+    public static final String TICKERS_TABLE = "tickers";
 
     public TickerRepository(SQLiteDatabase database) {
         this.database = database;
@@ -22,11 +26,11 @@ public class TickerRepository {
     }
 
     private void insertCryptocoinIfNotExists(Cryptocoin cc) {
-        content.put("id_string", cc.getId());
+        content.put("id_string", cc.getIdString());
         content.put("name", cc.getName());
         content.put("symbol", cc.getSymbol());
 
-        database.insertWithOnConflict("cryptocoins", null, content, SQLiteDatabase.CONFLICT_IGNORE);
+        database.insertWithOnConflict(CRYPTOCOINS_TABLE, null, content, SQLiteDatabase.CONFLICT_IGNORE);
         content.clear();
     }
 
@@ -68,7 +72,7 @@ public class TickerRepository {
                 content.put("percent_change_7d", t.getPercentChange7d());
                 content.put("last_update_timestamp", t.getLastUpdated());
 
-                database.insertWithOnConflict("tickers", null, content, SQLiteDatabase.CONFLICT_REPLACE);
+                database.insertWithOnConflict(TICKERS_TABLE, null, content, SQLiteDatabase.CONFLICT_REPLACE);
                 content.clear();
             }
             
@@ -82,10 +86,11 @@ public class TickerRepository {
         StringBuilder sql = new StringBuilder();
         sql.append("SELECT rank, price_usd, price_btc, volume_usd_24h, ")
                 .append("market_cap_usd, available_supply, total_supply, percent_change_1h, percent_change_24h, percent_change_7d, ")
-                .append("last_update_timestamp ")
+                .append("last_update_timestamp, cryptocoins.id, cryptocoins.id_string, cryptocoins.name, cryptocoins.symbol, ")
+                .append("cryptocoins.favorite ")
                 .append("FROM tickers ")
                 .append("INNER JOIN cryptocoins ")
-                .append("ON tickers.id_cryptocoin = cryptocoins.id");
+                .append("ON tickers.id_cryptocoin = cryptocoins.id ");
 
         Cursor cursor = database.rawQuery(sql.toString(), null);
 
@@ -100,10 +105,10 @@ public class TickerRepository {
                     cc = new Cryptocoin(cursor.getInt(11),cursor.getString(12), cursor.getString(13), cursor.getString(14));
                     cc.setIsFavorite(currentFavorite);
 
-                    currentTicker = new Ticker(cc , cursor.getInt(3), cursor.getDouble(4), cursor.getDouble(5),
+                    currentTicker = new Ticker(cc , cursor.getInt(0), cursor.getDouble(1), cursor.getDouble(2),
+                            cursor.getDouble(3), cursor.getDouble(4), cursor.getDouble(5),
                             cursor.getDouble(6), cursor.getDouble(7), cursor.getDouble(8),
-                            cursor.getDouble(9), cursor.getDouble(10), cursor.getDouble(11),
-                            cursor.getDouble(12), cursor.getLong(13));
+                            cursor.getDouble(9), cursor.getLong(10));
 
                     result.add(currentTicker);
                 } while (cursor.moveToNext());
@@ -113,44 +118,27 @@ public class TickerRepository {
         return result;
     }
 
-    /*private Integer selectIdPrimaryByStringId(String idString) {
-        StringBuilder sql = new StringBuilder();
-        Integer id = null;
-        String[] params = {idString};
-
-        sql.append("SELECT id ")
-                .append("FROM cryptocoins ")
-                .append("WHERE id_string = ?");
-
-        Cursor cursor = this.database.rawQuery(sql.toString(), params);
-        if(cursor != null) {
-            cursor.moveToFirst();
-            id =  new Integer(cursor.getInt(0));
-        }
-        cursor.close();
-
-        return id;
-    }*/
-
     public boolean favoriteACryptocoin(Integer id) {
         if(id == null) {
             return false;
         }
+        StringBuilder whereClause = new StringBuilder();
+        whereClause.append("id = ").append(id.intValue());
         ContentValues content = new ContentValues();
-        content.put("id_cryptocoin", id.intValue());
-        this.database.insertWithOnConflict("favorite_cryptocoins", null, content, SQLiteDatabase.CONFLICT_ABORT);
+        content.put("favorite", FAVORITE_TRUE);
+        this.database.update(CRYPTOCOINS_TABLE, content, whereClause.toString(), null);
         return true;
     }
 
     public boolean unFavoriteACryptocoin(Integer id) {
-        StringBuilder whereClause = new StringBuilder();
         if(id == null) {
             return false;
         }
-        whereClause.append("id_cryptocoin = " + id.intValue());
-
-        this.database.delete("favorite_cryptocoins", whereClause.toString(), null);
-
+        StringBuilder whereClause = new StringBuilder();
+        whereClause.append("id = ").append(id.toString());
+        ContentValues content = new ContentValues();
+        content.put("favorite", FAVORITE_FALSE);
+        this.database.update(CRYPTOCOINS_TABLE, content, whereClause.toString(), null);
         return true;
     }
 
